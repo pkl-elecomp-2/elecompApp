83893e6fffa05d3dc3a8e07fd6a8a2a3126754e8
Unero <20809454+un-ro@users.noreply.github.com>
2021-07-13 14:50:57 +0700

    Delete default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tab/tab.module').then( m => m.TabPageModule)
-  },
-  {
-    path: 'beranda',
-    loadChildren: () => import('./pages/beranda/beranda.module').then( m => m.BerandaPageModule)
-  },
-  {
-    path: 'member',
-    loadChildren: () => import('./pages/member/member.module').then( m => m.MemberPageModule)
-  },
-  {
-    path: 'artikel',
-    loadChildren: () => import('./pages/artikel/artikel.module').then( m => m.ArtikelPageModule)
-  },
-  {
-    path: 'profil',
-    loadChildren: () => import('./pages/profil/profil.module').then( m => m.ProfilPageModule)
-  },
+  }
 ];
 
 @NgModule({
