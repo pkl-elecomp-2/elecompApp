c69bb7de2a6fd30e74decf5a6fed61091b68760e
fahmi0604 <fahmifirmansyah917@gmail.com>
2021-07-21 09:01:48 +0700

    add formatDate

--- src/app/pages/artikel/artikel.page.ts
+++ src/app/pages/artikel/artikel.page.ts
@@ -80,4 +80,18 @@ export class ArtikelPage implements OnInit {
     });
   }
 
+  formatDate(date) {
+    const d = new Date(date);
+        let month = '' + (d.getMonth() + 1);
+        let day = '' + d.getDate();
+        const year = d.getFullYear();
+
+    if(month.length < 2)
+        {month = '0' + month;}
+    if(day.length < 2)
+        {day = '0' + day;}
+
+    return [day, month, year].join('-');
+}
+
 }
