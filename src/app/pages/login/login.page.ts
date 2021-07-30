510e0c03cdcad8879a686db4c93c8a5fb1052ab3
Unero <20809454+un-ro@users.noreply.github.com>
2021-07-30 16:26:33 +0700

    ✨ Login Feature Fix
    Now login can save User cred as Object

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,8 +3,6 @@ import { ToastService } from './../../services/toast.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -28,7 +26,6 @@ export class LoginPage implements OnInit {
   // Login
   onLogin() {
     // Check if username and password is empty
-    console.log(`${typeof(this.username)} with value ${this.username}`);
     if (this.username === undefined || this.password === undefined) {
       this.toastService.showError('Username atau Password tidak boleh kosong');
     } else {
@@ -36,7 +33,7 @@ export class LoginPage implements OnInit {
         if (res.status === 'success') {
           this.toastService.showSuccess(res.message);
           this.reRoute();
-          this.saveStorage(this.username, this.isChecked);
+          this.saveStorage(this.username, this.isChecked, res.data);
         } else {
           this.toastService.showError(res.message);
         }
@@ -45,9 +42,10 @@ export class LoginPage implements OnInit {
   }
 
   // Save to Local Storage
-  private saveStorage(user: string, isChecked: boolean) {
+  private saveStorage(user: string, isChecked: boolean, dataUser: any) {
     this.storage.set('user', user);
     this.storage.set('isChecked', isChecked);
+    this.storage.set('dataUser', dataUser[0]);
   }
 
   // When Login success redirect to
